1fab0ca27cf8a15ef29e2b31c1d6bfbf70e92aa8
icrmahin <icrmahin@gmail.com>
2026-09-17 17:35:02 +0600

    uu

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup.
 require("config.remote_clipboard").setup()
 
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.g.autoformat = false
+vim.opt.cursorline =  false
